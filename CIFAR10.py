8ef894770ff6a84eef84bcee07a9599150a81b31
iVishalr <vishalramesh01@gmail.com>
2021-05-20 16:16:09 +0530

    Bug Fixes and Improvements

--- CIFAR10.py
+++ CIFAR10.py
@@ -1,6 +1,5 @@
 import torch
 import numpy as np
-
 from trainer import TrainingConfig, Trainer
 from dataset import CIFAR10
 from model import ConvNet
@@ -26,7 +25,7 @@ test_set = CIFAR10(root="./cifar-10-batches-py",train=False,
 train_config = TrainingConfig(max_epochs=100,
                               lr=0.00023570926966106847,
                               weight_decay=0.00021257445443209662,
-                              ckpt_path="./CIFAR10.pt",
+                              ckpt_path="./models/CIFAR10.pt",
                               batch_size=64,
                               num_workers=4)
 
@@ -34,21 +33,23 @@ train_config = TrainingConfig(max_epochs=100,
 if __name__ == "__main__":
 
     model = ConvNet()
-    model.load_state_dict(torch.load("./ConvNet_Model1.pt"))
+    model.load_state_dict(torch.load("./Final_Model.pt")) #Uncomment this if you want to resume training process. Makesure to change the model name 
     trainer = Trainer(model,train_dataset=train_set,
                     test_dataset=test_set,config=train_config)
 
-    trainer.train_losses = torch.load("./train_losses.pt")
-    trainer.train_accuracies = torch.load("./train_accuracies.pt")
-    trainer.test_losses = torch.load("./test_losses.pt")
-    trainer.test_accuracies = torch.load("./test_accuracies.pt")
+    trainer.train_losses = torch.load("./log/train_losses.pt")
+    trainer.train_accuracies = torch.load("./log/train_accuracies.pt")
+    trainer.test_losses = torch.load("./log/test_losses.pt")
+    trainer.test_accuracies = torch.load("./log/test_accuracies.pt")
 
     trainer.train()
 
-    torch.save(model.state_dict(),"./Model600.pt")
-    torch.save(trainer.train_losses,"./train_losses2.pt")
-    torch.save(trainer.train_accuracies,"./train_accuracies2.pt")
-    torch.save(trainer.test_losses,"./test_losses2.pt")
-    torch.save(trainer.test_accuracies,"./test_accuracies2.pt")
+    torch.save(model.state_dict(),"./models/Model.pt")
+    torch.save(trainer.train_losses,"./log/train_losses2.pt")
+    torch.save(trainer.train_accuracies,"./log/train_accuracies2.pt")
+    torch.save(trainer.test_losses,"./log/test_losses2.pt")
+    torch.save(trainer.test_accuracies,"./log/test_accuracies2.pt")
+    
+    #Learning rates and weight_decay used
     # lr = 0.0009446932175584296 -> 0.00023570926966106847
-    # reg = 0.00011257445443209662 -> 2.6444459146597265e-05
+    # reg = 0.00011257445443209662 -> 0.00021257445443209662
